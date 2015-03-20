a7f946c33b677c08f76525ee14cd05924e48b7c3
Tobias Hauck <tobias.hauck@aoe.com>
2015-03-20 22:38:12 +0100

    changed license header for each class

--- Tests/Functional/Traits/TestingRoutes.php
+++ Tests/Functional/Traits/TestingRoutes.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * This file is part of GPL.
+ * This file is part of CiRestClientBundle.
  *
- * GPL is free software: you can redistribute it and/or modify
+ * CiRestClientBundle is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * GPL is distributed in the hope that it will be useful,
+ * CiRestClientBundle is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GPL.  If not, see <http://www.gnu.org/licenses/>.
+ * along with CiRestClientBundle.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Ci\RestClientBundle\Tests\Functional\Traits;
